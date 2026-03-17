efff3cdf0229cd4465294386a0b3691bf33883f5
JefersonOliveira <jeferson.santos260802@gmail.com>
2026-03-16 22:42:21 -0300

    Aula 02 variaveis!

---
+++ Aula02-variaveis.py
@@ -0,0 +1,24 @@
+print("Ola, Mundo da Morte ")
+
+print(7+4)
+print("7+4")
+print("7" + "4") # contena strings
+
+# Comentariode 1 linha no python
+
+'''
+Comentario de 
+multiplas 
+linhas
+'''
+#variaveis
+nome = "jeferson"
+idade = 26 # int
+peso = 70.2 #float
+print(nome, idade, peso)
+
+#input - Simular um Formulario no CDM
+nome = input("digite seu nome:") #string
+Idade = int(input("digite sua idade:"))
+peso = float(input("digite seu peso ")
+
---
+++ Desafios.py
@@ -0,0 +1,8 @@
+from numba.core.ir import Print
+
+nome = input('digite seus nome:')
+print(f"seja bem vindo {nome}")
+
+dia=int(input("digite o mes:"))
+mes=int(input("digite seu mes:"))
+ano=int(input("digite seu ano:"))
